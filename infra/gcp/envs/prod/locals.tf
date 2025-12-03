9ad0b0b8b99143b08b3ac0c8aeed69beb10ceba4
jmlarios <jm.larios06@gmail.com>
2025-12-03 18:51:37 +0100

    Remove GCP env/prod terraform files

--- infra/gcp/envs/prod/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  env_code              = lower(var.environment)
-  project_suffix_normal = "${local.env_code}-${var.project_suffix}"
-
-  # GCP project configuration
-  project_id   = "devops-garmenta-${local.project_suffix_normal}"
-  project_name = "Devops Garmenta ${title(local.env_code)} ${var.project_suffix}"
-
-}
